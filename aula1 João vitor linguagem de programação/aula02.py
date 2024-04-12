35dbd1c52cb72127555a1cd90b1d78705846da0a
João vitor <jv2555175@gmail.com>
2024-04-11 21:49:05 -0300

    Update,python
    Fiz a atividade do dia 11/04/2024

---
+++ aula1 João vitor linguagem de programação/aula02.py
@@ -0,0 +1,12 @@
+#print("Olá Mundo, finalmente quebrei a maldição")
+
+#um commentario
+
+print("Olá bem vindo ao nosso app!")
+
+nome  = input("Qual é o seu nome ?:")
+idade = input("Qual é a sua idade ?:")
+gosto = input("Qual é o seu hobbie ?:")
+
+print(f"Olá {nome}, você tem {idade}anos , Seu {gosto}")
+#print("Seu nome é David, sua idade é 25 anos. gosta de bateria")
